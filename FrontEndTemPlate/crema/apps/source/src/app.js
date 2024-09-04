a56da233898397651499d5ac24c2be2005a5a1ab
thang0296 <thang0296@gmail.com>
2024-09-04 12:57:25 +0700

    1

---
+++ FrontEndTemPlate/crema/apps/source/src/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import AppContextProvider from '@crema/context/AppContextProvider';
+import AppThemeProvider from '@crema/context/AppThemeProvider';
+import AppLocaleProvider from '@crema/context/AppLocaleProvider';
+import AppAuthProvider from './core/AppAuthProvider';
+import AuthRoutes from '@crema/components/AuthRoutes';
+import AppLayout from './core/AppLayout';
+import '@crema/mockapi';
+import { GlobalStyles } from './core/theme/GlobalStyle';
+import { Normalize } from 'styled-normalize';
+// ví dụ file này giwof code thêm chức năng vào đây chẳng hạn đó như t comment này cũng à thay đổi nội dung file ok
+// giờ push file lên
+// nó báo là k có gì thay đổi
+// vì m chưa commit
+const App = () => (
+  <AppContextProvider>
+    <AppThemeProvider>
+      <AppLocaleProvider>
+        <BrowserRouter>
+          <AppAuthProvider>
+            <AuthRoutes>
+              <GlobalStyles />
+              <Normalize />
+              <AppLayout />
+            </AuthRoutes>
+          </AppAuthProvider>
+        </BrowserRouter>
+      </AppLocaleProvider>
+    </AppThemeProvider>
+  </AppContextProvider>
+);
+
+export default App;
